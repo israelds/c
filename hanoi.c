086a46f9d4ba8bdc0995a3b226d93c01de095f50
Israel D de Souza <israel.diassouza@gmail.com>
2022-05-01 00:59:38 -0300

    implementando a funcao de exibir o menu

--- hanoi.c
+++ hanoi.c
@@ -1,14 +1,38 @@
+/*
+ * Author: Israel Souza <israel.diassouza@gmail.com>
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 
-void showMenu(void);
+int menu(void);
 
 
 void main(void) {
-    showMenu();
+    int menuOption = 0;
+    menuOption = menu();
+    printf("Option %d\n", menuOption);
+
+    switch(menuOption) {
+        case 1:
+            printf("The Rules are: ");
+            break;
+        case 2: 
+            printf("Let's play\n");
+            break;
+        case 0: 
+            exit(0);
+    }
 }
 
 
-void showMenu(void) {
-    printf("It will show a menu");
+int menu(void) {
+    int option = 0;
+    printf("1 - Show the Rules\n");
+    printf("2 - Play\n");
+    printf("0 - Quit\n");
+
+    scanf("%d", &option);
+
+    return option;
 }
