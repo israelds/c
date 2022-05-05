bc8d3d6215df317154cd02cbbd359d2db38af43a
Israel <israel.diassouza@gmail.com>
2022-05-05 00:26:14 -0300

    Inclusao dos metodos selectLevel e clear

--- hanoi.c
+++ hanoi.c
@@ -4,35 +4,54 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+// #include <curses.h>
 
+int rods[3][0];
+
+void clear(void);
 int menu(void);
-
+int selectLevel(void);
 
 void main(void) {
+    clear();
     int menuOption = 0;
+    int level = -1;
     menuOption = menu();
-    printf("Option %d\n", menuOption);
 
     switch(menuOption) {
         case 1:
             printf("The Rules are: ");
             break;
         case 2: 
-            printf("Let's play\n");
+            level = selectLevel(); 
             break;
-        case 0: 
+        case 0:
+            clear();
             exit(0);
     }
 }
 
-
 int menu(void) {
+    clear();
     int option = 0;
     printf("1 - Show the Rules\n");
     printf("2 - Play\n");
     printf("0 - Quit\n");
-
     scanf("%d", &option);
-
     return option;
+}
+
+int selectLevel(void) {
+    int level = -1;
+    clear();
+    printf("1 - Easy\n");
+    printf("2 - Intermediate\n");
+    printf("3 - Hard\n");
+    printf("4 - Custom\n");
+    scanf("%d", &level);
+    return level;
+}
+
+void clear(void) {
+    system("clear");
 }
